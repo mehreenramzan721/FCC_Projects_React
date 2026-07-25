cecc695393b53cc37e6978b6988e662a01894087
mehreenramzan721 <mehreenramzan721@gmail.com>
2026-07-25 20:14:35 +0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Accordian from "./components/accordian";
 import RandomColor from "./components/random-color";
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-data';
 
 
 import './App.css'
@@ -15,11 +16,13 @@ function App() {
       {/* <RandomColor/> */}
       {/* <StarRating noOfStars={10}/> */}
 
-      <ImageSlider
+      {/* <ImageSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
-        limit={"10"}
-      />
+        limit={"10"} 
+       /> */}
+             <LoadMoreData/>
+
     </>
   )
 }
