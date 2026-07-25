0ed868de06ad9e494c25f8ad941c70d06fed1caf
mehreenramzan721 <mehreenramzan721@gmail.com>
2026-07-25 13:57:53 +0500

    remove unnecesaay stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import Accordian from './components/accordian'
+
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
