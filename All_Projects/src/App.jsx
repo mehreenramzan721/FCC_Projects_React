2b92825769a9a6446a1e0e1892d47919fd7c42b9
mehreenramzan721 <mehreenramzan721@gmail.com>
2026-07-27 21:09:46 +0500

    commits

--- All_Projects/src/App.jsx
+++ All_Projects/src/App.jsx
@@ -1,41 +1,72 @@
-import { useState } from 'react'
-import Accordian from "./components/accordian";
-import RandomColor from "./components/random-color";
-import StarRating from './components/star-rating';
-import ImageSlider from './components/image-slider';
-import LoadMoreData from './components/load-more-data';
-import TreeView from './components/tree-view';
-import menus from './components/tree-view/data';
-import QRCodeGenerator from './components/qr-code-generator'
-import ThemeChanger from './components/light-dark-mode'
-import ScrollIndicator from './components/scroll-indicator'
-import TabTest from "./components/custom-tabs/tab-test";
+// import { useState } from 'react'
+// import Accordian from "./components/accordian";
+// import RandomColor from "./components/random-color";
+// import StarRating from './components/star-rating';
+// import ImageSlider from './components/image-slider';
+// import LoadMoreData from './components/load-more-data';
+// import TreeView from './components/tree-view';
+// import menus from './components/tree-view/data';
+// import QRCodeGenerator from './components/qr-code-generator'
+// import ThemeChanger from './components/light-dark-mode'
+// import ScrollIndicator from './components/scroll-indicator'
+// import TabTest from "./components/custom-tabs/tab-test"
+// import ScrollToSection from './components/scroll-indicator'
+// import ScrollToTopAndBottom from './components/scroll-to-top-and-bottom'
+// import TicTacToe from './components/tic-tac-toe'
+// import SearchAutocomplete from './components/search-autocomplete-with-api'
+// import UseFetchHookTest from './components/use-fetch'
+// import UseOnclickOutsideTest from './components/use-outside-click'
+// import ModalTest from './components/custom-modal-popup'
+// import UseWindowResizeTest from './components/use-window-resize'
 
-import './App.css'
 
-function App() {
+// import './App.css'
 
-  return (
-    <>
-      {/* <Accordian/> */}
-      {/* <RandomColor/> */}
-      {/* <StarRating noOfStars={10}/> */}
+// function App() {
 
-      {/* <ImageSlider
-        url={"https://picsum.photos/v2/list"}
-        page={"1"}
-        limit={"10"} 
-       /> */}
-      {/* <LoadMoreData /> */}
+//   return (
+//     <>
+//       {/* <Accordian/> */}
+//       {/* <RandomColor/> */}
+//       {/* <StarRating noOfStars={10}/> */}
 
-      {/* <TreeView menus={menus} /> */}
-      {/* <QRCodeGenerator /> */}
-      {/* <ThemeChanger /> */}
-      <ScrollIndicator url={"https://dummyjson.com/products?limit=100"}/>
-            <TabTest />
+//       {/* <ImageSlider
+//         url={"https://picsum.photos/v2/list"}
+//         page={"1"}
+//         limit={"10"} 
+//        /> */}
+//       {/* <LoadMoreData /> */}
 
-    </>
-  )
-}
+//       {/* <TreeView menus={menus} /> */}
+//       {/* <QRCodeGenerator /> */}
+//       {/* <ThemeChanger /> */}
+//       <ScrollIndicator url={"https://dummyjson.com/products?limit=100"}/>
+//       {/* <TabTest /> */}
 
-export default App
+//       {/* <ModalTest/> */}
+
+//       {/* <GithubProfileFinder/>
+
+//       <SearchAutocomplete/>
+
+//       <TicTacToe/> */}
+
+//       {/* <FeatureFlagGlobalState>
+//         <FeatureFlags />
+//       </FeatureFlagGlobalState> */}
+
+//       {/* <UseFetchHookTest/> */}
+
+//       {/* <UseOnclickOutsideTest/>  */}
+
+//       {/* <UseWindowResizeTest/> */}
+
+//       {/* <ScrollToTopAndBottom/> */}
+
+//       <ScrollToSection/>
+
+//     </>
+//   )
+// }
+
+// export default App
